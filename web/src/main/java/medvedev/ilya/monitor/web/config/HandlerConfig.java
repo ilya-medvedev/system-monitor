e29826d0f3b978175502689bc3b3d18991e3c57a
Ilya Medvedev <medved55rus@gmail.com>
2016-06-13 02:03:59 +0600

    spring config refactoring

--- web/src/main/java/medvedev/ilya/monitor/web/config/HandlerConfig.java
+++ web/src/main/java/medvedev/ilya/monitor/web/config/HandlerConfig.java
@@ -4,23 +4,14 @@ import medvedev.ilya.monitor.sensor.Sensor;
 import medvedev.ilya.monitor.sensor.cpu.Cpu;
 import medvedev.ilya.monitor.sensor.mem.Mem;
 import medvedev.ilya.monitor.web.handler.Handler;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.WebSocketHandler;
 
-import java.io.File;
-
 @Configuration
 public class HandlerConfig {
     @Bean
-    public WebSocketHandler webSocketHandler(
-            @Value("${sensors.cpu.file}") final File cpuFile,
-            @Value("${sensors.mem.file}") final File memFile
-    ) {
-        final Cpu cpu = Cpu.byFile(cpuFile);
-        final Mem mem = Mem.byFile(memFile);
-
+    public WebSocketHandler webSocketHandler(final Cpu cpu, final Mem mem) {
         final Sensor[] sensors = new Sensor[] {cpu, mem};
 
         return new Handler(sensors);
---
+++ web/src/main/java/medvedev/ilya/monitor/web/config/SensorConfig.java
@@ -0,0 +1,22 @@
+package medvedev.ilya.monitor.web.config;
+
+import medvedev.ilya.monitor.sensor.cpu.Cpu;
+import medvedev.ilya.monitor.sensor.mem.Mem;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.File;
+
+@Configuration
+public class SensorConfig {
+    @Bean
+    public Cpu cpu(@Value("${sensors.cpu.file}") final File file) {
+        return Cpu.byFile(file);
+    }
+
+    @Bean
+    public Mem mem(@Value("${sensors.mem.file}") final File file) {
+        return Mem.byFile(file);
+    }
+}
