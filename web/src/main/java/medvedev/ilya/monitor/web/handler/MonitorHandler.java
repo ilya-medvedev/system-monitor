40dff2b4a46ce8c9be7f79b018c512ad0442742d
Ilya Medvedev <medved55rus@gmail.com>
2016-06-12 17:37:06 +0600

    akka actors for messaging

--- web/src/main/java/medvedev/ilya/monitor/web/handler/MonitorHandler.java
+++ web/src/main/java/medvedev/ilya/monitor/web/handler/MonitorHandler.java
@@ -1,11 +1,15 @@
 package medvedev.ilya.monitor.web.handler;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
 import medvedev.ilya.monitor.proto.Protobuf.Message;
+import medvedev.ilya.monitor.proto.Protobuf.Message.SensorInfo;
 import medvedev.ilya.monitor.sensor.Sensor;
 import medvedev.ilya.monitor.sensor.cpu.Cpu;
 import medvedev.ilya.monitor.sensor.mem.Mem;
 import medvedev.ilya.monitor.util.ExceptionHandler;
-import medvedev.ilya.monitor.web.session.AsyncSession;
+import medvedev.ilya.monitor.web.actor.SessionActor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.socket.BinaryMessage;
@@ -15,19 +19,24 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.AbstractWebSocketHandler;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class MonitorHandler extends AbstractWebSocketHandler {
     private static final Logger LOGGER = LoggerFactory.getLogger(MonitorHandler.class);
 
+    private static final ActorRef NO_SENDER = ActorRef.noSender();
+
     private final Sensor[] sensors;
 
-    private final Map<WebSocketSession, AsyncSession> sessions = new ConcurrentHashMap<>();
+    private final ActorSystem system = ActorSystem.create();
+    private final Map<WebSocketSession, ActorRef> actors = new ConcurrentHashMap<>();
 
     private final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
     private final Runnable runnable;
@@ -44,46 +53,47 @@ public class MonitorHandler extends AbstractWebSocketHandler {
 
     @Override
     public synchronized void afterConnectionEstablished(final WebSocketSession session) {
-        if (sessions.isEmpty()) {
+        if (actors.isEmpty()) {
             scheduledFuture = executorService.scheduleAtFixedRate(runnable, 0, 1, TimeUnit.SECONDS);
         }
 
-        final AsyncSession asyncSession = new AsyncSession(session);
+        final ActorRef actorRef = system.actorOf(Props.create(SessionActor.class, session));
 
-        sessions.put(session, asyncSession);
+        actors.put(session, actorRef);
     }
 
     private void sendStats() {
         final long time = System.currentTimeMillis();
 
-        final Message.Builder builder = Message.newBuilder()
-                .setTime(time);
-
-        Arrays.stream(sensors)
+        final List<SensorInfo> sensorInfoList = Arrays.stream(sensors)
                 .parallel()
                 .unordered()
                 .map(Sensor::sensorInfo)
-                .forEach(builder::addValue);
+                .collect(Collectors.toList());
 
-        final byte[] bytes = builder
+        final byte[] bytes = Message.newBuilder()
+                .setTime(time)
+                .addAllValue(sensorInfoList)
                 .build()
                 .toByteArray();
 
-        sessions.values()
+        actors.values()
                 .parallelStream()
                 .unordered()
-                .forEach(session -> {
+                .forEach(actorRef -> {
                     final WebSocketMessage message = new BinaryMessage(bytes);
 
-                    session.send(message);
+                    actorRef.tell(message, NO_SENDER);
                 });
     }
 
     @Override
     public synchronized void afterConnectionClosed(final WebSocketSession session, final CloseStatus status) {
-        sessions.remove(session);
+        final ActorRef actorRef = actors.remove(session);
 
-        if (sessions.isEmpty()) {
+        system.stop(actorRef);
+
+        if (actors.isEmpty()) {
             scheduledFuture.cancel(true);
 
             Arrays.stream(sensors)
